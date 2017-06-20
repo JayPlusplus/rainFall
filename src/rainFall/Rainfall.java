4a898e75064f9eb0cf2e316a1e33c018a966d24b
Jay Wu <wu274319426@gmail.com>
2017-06-20 11:33:28 -0400

    rainFall

--- src/rainFall/Rainfall.java
+++ src/rainFall/Rainfall.java
@@ -5,17 +5,33 @@ public class Rainfall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double num1, num2,num3;
+		double length, width,thickness;
+		
+		
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("length");
-		num1 = keyboard.nextDouble();
+		length = keyboard.nextDouble();
 		System.out.println("Width");
-		num2 = keyboard.nextDouble();
+		width = keyboard.nextDouble();
 		System.out.println("thickness");
-		num3 = keyboard.nextDouble();
-		System.out.println(num1 * num2 * num3);
+		thickness = keyboard.nextDouble();
+		System.out.println(length * width * thickness);
 		
 		
+	    
+		if (length > 0 && length <= 300){
+			//length is between 0 - 300
+			
+		System.out.print("it is rainfall!");
+		}
+		else if (length > 300 && length <= 600){
+			//length is bigger than 300 and less or equal than 600
+			System.out.print("it's pouring!");	
+		}
+		else { 
+			//not possible condition
+			System.out.print("it's impossible!");
+		}
 	}
 
 }
